d5c338dfcfb78afa9072d6355725d38458b4368e
VoltorH <darkkrasher@gmail.com>
2024-01-30 18:35:20 +0100

    Hitboxes varias + gameover (falta positivo/negativo)

---
+++ Assets/MenuController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Test room");
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+
+        }
+    }
+}
