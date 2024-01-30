94b1b7ef0a7d23a4d0f188d83a72357ac376fef7
VoltorH <darkkrasher@gmail.com>
2024-01-30 18:09:17 +0100

    GameOver Scene

---
+++ Assets/GameOver.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public bool _isDead;
+    private Rigidbody2D _rb;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _rb = collision.GetComponent<Rigidbody2D>();
+        if (_rb != null)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+}
