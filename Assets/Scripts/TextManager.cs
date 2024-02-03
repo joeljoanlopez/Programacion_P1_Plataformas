2cdd5ae6a5ccb562bb72a2b18d04f5e0e63cbf26
Joloharo <joeljoanlopez@gmail.com>
2024-02-03 20:24:35 +0100

    Mapa terminado y adaptado

--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -6,24 +6,14 @@ using UnityEngine.UI;
 public class TextManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Text _text;
 
-    void Start()
+    private void Start()
     {
-        _text = GetComponent<Text>();
+        GetComponent<Text>().text = GameOver._text;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-   
-        if (PlayerPrefs.GetInt("final") == 0)
-        {
-            _text.text = ("You died");
-        }
-        if (PlayerPrefs.GetInt("final") == 1 )
-        {
-            _text.text = ("You win");
-        }
     }
 }
